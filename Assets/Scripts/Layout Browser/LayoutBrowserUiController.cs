ab7f62a5ae25300dc8c08f7e96d10efad7a94299
fireMCG <maximecloutiergravel@gmail.com>
2026-02-03 20:48:26 -0500

    Main Menu and Act Browser Ui

--- Assets/Scripts/Layout Browser/LayoutBrowserUiController.cs
+++ Assets/Scripts/Layout Browser/LayoutBrowserUiController.cs
@@ -1,6 +1,11 @@
+using fireMCG.PathOfLayouts.Messaging;
 using UnityEngine;
 
 public class LayoutBrowserUiController : MonoBehaviour
 {
-
+    public void OpenMainMenu()
+    {
+        OnAppStateChangeRequest message = new OnAppStateChangeRequest(StateController.AppState.MainMenu);
+        MessageBusManager.Resolve.Publish(message);
+    }
 }
--- Assets/Scripts/Main Menu/MainMenuUiController.cs
+++ Assets/Scripts/Main Menu/MainMenuUiController.cs
@@ -1,6 +1,39 @@
+using fireMCG.PathOfLayouts.Messaging;
 using UnityEngine;
 
 public class MainMenuUiController : MonoBehaviour
 {
+    [field: SerializeField] private GameObject _settingsWindow;
 
+    private void Awake()
+    {
+        SetSettingsWindowState(false);
+    }
+
+    public void OpenLayoutBrowser()
+    {
+        OnAppStateChangeRequest message = new OnAppStateChangeRequest(StateController.AppState.LayoutBrowser);
+        MessageBusManager.Resolve.Publish(message);
+    }
+
+    public void OpenLearningCenter()
+    {
+        OnAppStateChangeRequest message = new OnAppStateChangeRequest(StateController.AppState.LearningCenter);
+        MessageBusManager.Resolve.Publish(message);
+    }
+
+    public void ToggleSettingsWindow()
+    {
+        SetSettingsWindowState(!_settingsWindow.activeSelf);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void SetSettingsWindowState(bool state)
+    {
+        _settingsWindow.SetActive(state);
+    }
 }
